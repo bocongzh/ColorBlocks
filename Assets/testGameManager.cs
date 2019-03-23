d450f958b6de5b0cab918597a8dda353740fb404
Bertram <bertram.zbc@gmail.com>
2019-03-23 16:13:26 -0700

    inital

---
+++ Assets/testGameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using System;
+
+public class testGameManager : MonoBehaviourPun
+{
+
+
+    int num = 0;
+    public GameObject player;
+    public GameObject body;
+
+    Vector3[] startPositions = { new Vector3(-16, 13, 0), new Vector3(20, 13, 0), new Vector3(-16, -13, 0), new Vector3(20, -13, 0) };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int playerNum = PhotonNetwork.CurrentRoom.PlayerCount;
+        PhotonNetwork.Instantiate(playerNum+"_head", startPositions[playerNum - 1], Quaternion.identity, 0);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
