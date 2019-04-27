c9ae999cecc95a6749fb25c9616a76160dc3f06b
Bertram <bertram.zbc@gmail.com>
2019-04-27 16:44:56 -0700

    update

---
+++ Assets/BodyPlayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class BodyPlayer : MonoBehaviourPunCallbacks, IPunObservable
+{
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(transform.name);
+        }
+        else
+        {
+            transform.name = (string)stream.ReceiveNext();
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
